f3f0d93f73424aac24d3d20e40a29ee6712b4809
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-03-24 10:15:28 +0100

    :lipstick: update: header & logo components

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,12 @@
 import Logo from "../logo/logo"
 import Nav from "../nav/nav"
-import LPLogo from "./../../assets/logos/logo-lp.svg"
+import LPLogo from "./../../assets/logos/logo-lp.webp"
 import mainNavLinkList from "../../data/main-nav-link-list"
 
 function Header() {
   return (
     <header className='flex justify-between items-center max-w-full max-h-20 p-4 sticky top-0 bg-white'>
-      <Logo image={LPLogo} className='max-w-20 max-h-12' />
+      <Logo image={LPLogo} className='max-w-20 max-h-10' />
       <Nav linkList={mainNavLinkList} className='fa-fw text-4xl' />
     </header>
   );
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,9 +1,7 @@
 import { cn } from "@/lib/utils"
 
-
-
 function Logo({ image, className }: ILogoProps) {
-  return <img src={image} alt="Lou Morgan René PIERRE" className={cn('', className)} />
+  return <a href="#"><img src={image} alt="Lou Morgan René PIERRE" className={cn('', className)} /></a>
 }
 
 export default Logo
