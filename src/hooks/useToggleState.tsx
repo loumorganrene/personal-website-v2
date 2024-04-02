3eefa21ee2baf10b29367ee7b174c0dde82c5f26
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-04-02 17:01:26 +0200

    :bug: fix: Popover toggle animation when clicking on the popover content

--- src/hooks/useToggleState.tsx
+++ src/hooks/useToggleState.tsx
@@ -1,26 +1,11 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState } from 'react'
 
 function useToggleState(initialState: boolean = false) {
   const [isToggled, setIsToggled] = useState(initialState)
-  const buttonRef = useRef<HTMLButtonElement>(null)
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (buttonRef.current && !buttonRef.current.contains(event.target as Node)) {
-        setIsToggled(false);
-      }
-    }
-
-    document.addEventListener('mousedown', handleClickOutside)
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    };
-  }, [])
 
   const toggle = () => setIsToggled(prevState => !prevState)
 
-  return { isToggled, toggle, buttonRef }
+  return { isToggled, toggle }
 }
 
 export default useToggleState
