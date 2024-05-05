8a423e7a792077cf4ed003799a3b744e14274604
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-05-05 03:50:34 +0200

    :coffin: update: remove scroll handler dead code

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,17 @@
 import Header from "./components/header/header"
 import { useSectionRefs } from "./hooks/useSectionRefs"
-import { handleScroll } from "./lib/utils"
 import ContactPage from "./pages/contact/contact-page"
 import EducationPage from "./pages/education/education-page"
 import FeedbacksPage from "./pages/feedbacks/feedbacks-page"
 import HomePage from "./pages/home/home-page"
 import ProjectsPage from "./pages/projects/projects-page"
 import { SectionRef } from "./types/enum"
-import ReactScrollWheelHandler from "react-scroll-wheel-handler"
 
 function App() {
   const sectionRefs = useSectionRefs()
 
   return (
     <>
-      <ReactScrollWheelHandler
-        className="scroll-wheel-handler scroll-m-0"
-        upHandler={() => handleScroll({ deltaY: -1 })}
-        downHandler={() => handleScroll({ deltaY: 1 })}
-      >
         <Header sectionRefs={sectionRefs} />
         <main>
           <HomePage />
@@ -27,7 +20,6 @@ function App() {
           <ContactPage ref={sectionRefs[SectionRef.Contact]} />
           <FeedbacksPage ref={sectionRefs[SectionRef.Feedbacks]} />
         </main>
-      </ReactScrollWheelHandler>
     </>
   );
 }
